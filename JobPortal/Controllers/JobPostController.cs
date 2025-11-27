e52a153496bd7eac4652b25d4b616413f3a86d01
Nebu Salim <siva01619@gmail.com>
2025-11-27 19:17:54 +0530

    .

---
+++ JobPortal/Controllers/JobPostController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JobPortal.Models;
+
+namespace JobPortal.Controllers
+{
+    public class JobPostController : Controller
+    {
+        jobportalEntities objdb = new jobportalEntities();
+        // GET: JobPost
+        public ActionResult JobPost_Pageload()
+        {
+            JobPost objCls = new JobPost();
+            objCls.MyQual = getQualificationData();
+            objCls.MySkill = getSkillData();
+            return View(objCls);
+        }
+        public List<checkBoxListChecker1> getQualificationData()
+        {
+            List<checkBoxListChecker1> sts = new List<checkBoxListChecker1>
+            {
+                new checkBoxListChecker1{Value="SSLC",Text="SSLC",isChecked=true},
+                new checkBoxListChecker1{Value="PLUS TWO",Text="PLUS TWO",isChecked=true},
+                new checkBoxListChecker1{Value="DIPLOMA",Text="DIPLOMA",isChecked=false},
+                new checkBoxListChecker1{Value="BTECH",Text="BTECH",isChecked=false},
+                new checkBoxListChecker1{Value="MTECH",Text="MTECH",isChecked=false}
+            };
+            return sts;
+        }
+        public List<checkBoxListChecker1> getSkillData()
+        {
+            List<checkBoxListChecker1> skl = new List<checkBoxListChecker1>
+            {
+                new checkBoxListChecker1{Value="C#",Text="C#",isChecked=true},
+                new checkBoxListChecker1{Value="ASP.NET",Text="ASP.NET",isChecked=false},
+                new checkBoxListChecker1{Value="MVC",Text="MVC",isChecked=false},
+                new checkBoxListChecker1{Value="CORE MVC",Text="CORE MVC",isChecked=false},
+                new checkBoxListChecker1{Value="Python",Text="Python",isChecked=false},
+                new checkBoxListChecker1{Value="JAVA",Text="JAVA",isChecked=false},
+            };
+            return skl;
+        }
+        public ActionResult JobPost_Click(JobPost objCls,FormCollection form)
+        {
+            if (ModelState.IsValid)
+            {
+                var qul = string.Join("/", objCls.selectedQual);
+                objCls.Qual = qul;
+                objCls.MyQual = getQualificationData();
+
+                var skl = string.Join(",", objCls.selectedSkill);
+                objCls.Skill = skl;
+                objCls.MySkill = getSkillData();
+                return View("JobPost_Pageload", objCls);
+                
+            }
+            else
+            {
+                objCls.MyQual = getQualificationData();
+                objCls.MySkill = getSkillData();
+                return View("JobPost_Pageload", objCls);
+            }            
+        }
+    }
+}
---
+++ JobPortal/Models/JobPost.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobPortal.Models
+{
+    public class checkBoxListChecker1
+    {
+        public string Value { set; get; }
+        public string Text { set; get; }
+        public bool isChecked { set; get; }
+    }
+    public class JobPost
+    {
+        [Required(ErrorMessage ="Enter the Job Title")]
+        public string JobTitle { set; get; }
+        [Required(ErrorMessage = "Enter the Job Description")]
+        public string JobDes { set; get; }
+        [Required(ErrorMessage = "Enter the Job Location")]
+        public string JobLoc { set; get; }
+
+        [Required(ErrorMessage = "Enter the Experience in Years")]
+        public int Experience { set; get; }
+
+        public List<checkBoxListChecker1> MySkill { set; get; }
+        public string[] selectedSkill { set; get; }
+        public string Skill { set; get; }
+
+        public List<checkBoxListChecker1> MyQual { set; get; }
+        public string[] selectedQual { set; get; }
+        public string Qual { set; get; }
+        [Required(ErrorMessage ="Enter Passout Year")]
+        [RegularExpression(@"^(19|20)\d{2}$",ErrorMessage ="Enter a Valid Year")]
+        public int PassOutYear { set; get; }
+        [Required(ErrorMessage = "Enter Start Date")]
+        public DateTime startDate { set; get; }
+        [Required(ErrorMessage = "Enter End Date")]
+        public DateTime endDate { set; get; }
+        [Required(ErrorMessage ="Enter Job Type")]
+        public string JobType { set; get; }
+    }
+}
